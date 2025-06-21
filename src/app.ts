45e9b668174d257f67b7a8ddfe8bb0d85fb5cd14
saidee-hasan <mdsaideehasan@gmail.com>
2025-06-21 22:26:23 +0600

    asserments done

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import { booksRoutes } from "./app/controllers/book.controller";
+import { borrowRoutes } from "./app/controllers/borrow.controller";
+
+const app: Application = express();
+app.use(express.json())
+
+app.use('/api/books', booksRoutes)
+app.use('/api/borrow', borrowRoutes)
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome from Library Management API')
+})
+
+// 404 error
+app.use((req: Request, res: Response) => {
+    res.status(404).json({ message: 'Sorry! Route not found' })
+})
+
+
+export default app;
